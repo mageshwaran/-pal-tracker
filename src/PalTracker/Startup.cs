01bed217f6478ef7bbb3220321bfe7038a1664bf
Mageshwaran <mageshwaran.82@gmail.com>
2019-09-09 14:24:45 +0530

    Externalized Configuration implemented

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -26,6 +26,13 @@ namespace PalTracker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton(sp => new WelcomeMessage(Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")));
+             services.AddSingleton(sp => new CloudFoundryInfo(
+                 Configuration.GetValue<string>("PORT", "9904"),
+                  Configuration.GetValue<string>("MemoryLimit", "512M"),
+                  Configuration.GetValue<string>("CfInstanceIndex", "1"),
+                  Configuration.GetValue<string>("CfInstanceAddr", "127.0.0.1"))
+                  );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
