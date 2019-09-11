a69903b6320fbf095ec6d2c1df2e5957e50696f4
Mageshwaran <mageshwaran.82@gmail.com>
2019-09-11 12:21:18 +0530

    Health monitor implementation

--- src/PalTracker/TimeEntryController.cs
+++ src/PalTracker/TimeEntryController.cs
@@ -1,26 +1,32 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-[Route("/time-entries")]
-public class TimeEntryController : ControllerBase
+namespace PalTracker
+{
+    [Route("/time-entries")]
+    public class TimeEntryController : ControllerBase
 {
     private readonly ITimeEntryRepository _timeEntryRepository;
+    private readonly IOperationCounter<TimeEntry> _operationCounter;
 
-    public TimeEntryController(ITimeEntryRepository timeEntryRepository)
+    public TimeEntryController(ITimeEntryRepository timeEntryRepository, IOperationCounter<TimeEntry> operationCounter)
     {
         this._timeEntryRepository = timeEntryRepository;
+        _operationCounter = operationCounter;
     }
 
     [HttpGet("{id}", Name = "GetTimeEntry")]
     public IActionResult  Read(long id)
     {
+        _operationCounter.Increment(TrackedOperation.Read);
+
         return _timeEntryRepository.Contains(id)? (IActionResult) Ok(_timeEntryRepository.Find(id)) : NotFound();
     }
 
     [HttpPost]
     public IActionResult  Create([FromBody] TimeEntry timeEntry)
     {
+        _operationCounter.Increment(TrackedOperation.Create);
+
         var createdTimeEntry = _timeEntryRepository.Create(timeEntry);
 
         return CreatedAtRoute("GetTimeEntry", new {id = createdTimeEntry.Id}, createdTimeEntry);
@@ -29,18 +35,21 @@ public class TimeEntryController : ControllerBase
     [HttpGet]
     public IActionResult  List()
     {
+        _operationCounter.Increment(TrackedOperation.List);
         return Ok(_timeEntryRepository.List());
     }
 
     [HttpPut("{id}")]
     public IActionResult  Update(long id, [FromBody] TimeEntry timeEntry)
     {
+        _operationCounter.Increment(TrackedOperation.Update);
         return _timeEntryRepository.Contains(id) ? (IActionResult) Ok(_timeEntryRepository.Update(id, timeEntry)) : NotFound();
     }
 
     [HttpDelete("{id}")]
     public IActionResult  Delete(long id)
     {
+        _operationCounter.Increment(TrackedOperation.Delete);
         if (!_timeEntryRepository.Contains(id))
         {
             return NotFound();
@@ -48,4 +57,5 @@ public class TimeEntryController : ControllerBase
         _timeEntryRepository.Delete(id);
         return NoContent();
     }
+}
 }
